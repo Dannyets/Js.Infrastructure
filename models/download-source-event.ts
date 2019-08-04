c8a92dee1d791201e5da9b5bed0abd0b8c266a13
dannyets <dannyets@gmail.com>
2019-08-04 11:59:36 +0300

    Remove unnecessary model

--- models/download-source-event.ts
+++
@@ -1,7 +0,0 @@
-import { Source } from "./source";
-import { Target } from './target';
-
-export interface DownloadSourceEvent {
-    source: Source;
-    target: Target;
-}
--- models/index.ts
+++ models/index.ts
@@ -3,7 +3,6 @@ export * from './entity';
 export * from './mongoEntity';
 export * from './repository';
 export * from './userRole';
-export * from './download-source-event';
 export * from './file';
 export * from './file-metadata';
 export * from './lambda-params';
