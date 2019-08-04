52431ea1406d01bcbd2e2504b47092f987d4735b
dannyets <dannyets@gmail.com>
2019-08-04 15:09:31 +0300

    Remvoe unneeded parameters in factories and update packge version to 1.2.3

--- src/factories/client.factory.ts
+++ src/factories/client.factory.ts
@@ -1,12 +1,12 @@
-import { Target, UploadClient, NotificationClient, ConfigKey, DownloadClient } from "../models";
+import { UploadClient, NotificationClient, ConfigKey, DownloadClient } from "../models";
 import { S3Client, SNSClient, LambdaClient, AuthClient } from "../clients";
 import { configUtils } from '../utils';
 
-const createUploadClient = (target: Target) : UploadClient | undefined => {
+const createUploadClient = () : UploadClient | undefined => {
     return new S3Client();
 }
 
-const createDownloadClient = (target: Target) : DownloadClient | undefined => {
+const createDownloadClient = () : DownloadClient | undefined => {
     return new S3Client();
 }
 
