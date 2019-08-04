dc511961d8f15b3df69e503546d325a10cd9fa94
dannyets <dannyets@gmail.com>
2019-08-04 15:06:58 +0300

    Added download client

--- src/factories/client.factory.ts
+++ src/factories/client.factory.ts
@@ -1,8 +1,12 @@
-import { Target, UploadClient, NotificationClient, ConfigKey } from "../models";
+import { Target, UploadClient, NotificationClient, ConfigKey, DownloadClient } from "../models";
 import { S3Client, SNSClient, LambdaClient, AuthClient } from "../clients";
 import { configUtils } from '../utils';
 
 const createUploadClient = (target: Target) : UploadClient | undefined => {
+    return new S3Client();
+}
+
+const createDownloadClient = (target: Target) : DownloadClient | undefined => {
     return new S3Client();
 }
 
@@ -22,6 +26,7 @@ const createAuthClient = () => {
 
 export default {
     createUploadClient,
+    createDownloadClient,
     createNotificationClient,
     createLambdaClient,
     createAuthClient
