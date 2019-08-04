831c6fe0cd96098b5851bfc22659bd54db36634f
dannyets <dannyets@gmail.com>
2019-08-04 12:07:54 +0300

    Merge config utils files

--- utils/config.utils.ts
+++ utils/config.utils.ts
@@ -1,3 +1,5 @@
+import AWS from "aws-sdk";
+
 declare const process: NodeJS.Process;
 
 function get<T>(key: string): T | undefined {
@@ -15,6 +17,19 @@ function get<T>(key: string): T | undefined {
     }
 }
 
+const configureAws = () => {
+    const config = {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+        region: process.env.AWS_REGION || 'us-east-1',
+    };
+
+    console.log(`Configuring AWS with args: ${JSON.stringify(config)}`);
+
+    AWS.config.update(config);
+}
+
 export default {
     get,
+    configureAws
 };
